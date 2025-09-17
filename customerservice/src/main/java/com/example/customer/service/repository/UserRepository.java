5ecc57fd34a2d2972ccb0e82f08bc7cd03039d36
SanidhyaMahesh <sanidhyam2005@gmail.com>
2025-09-17 16:05:36 +0530

    add

---
+++ customerservice/src/main/java/com/example/customer/service/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.customer.service.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+import com.example.customer.service.model.User;
+
+public interface UserRepository extends MongoRepository<User, String> {
+	User findByEmail(String email); 
+
+}
